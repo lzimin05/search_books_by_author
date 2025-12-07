d84535ec43b2e612729a55b2f6ca08b51d35ce8f
lzimin05 <lzimin05@mail.ru>
2025-12-07 15:54:30 +0300

    done l2

---
+++ service-b/src/main/java/com/bookservice/model/Book.java
@@ -0,0 +1,17 @@
+package com.bookservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private Integer year;
+    private Double price;
+}
